ab5915a5fbc36348b464981024ef8d064e643cb7
hinaschlippenbach <ahoahomen@gmail.com>
2014-02-09 00:28:29 +0900

    マインクラフトmod用オレオレライブラリ
    first commit

---
+++ hinasch_lib/lib/BWrapper.java
@@ -0,0 +1,23 @@
+package hinasch.lib;
+
+public class BWrapper {
+
+	private boolean bool;
+	
+	
+	public BWrapper(){
+		this.bool = false;
+	}
+	
+	public void setTrue(){
+		this.bool = true;
+	}
+	
+	public void setFalse(){
+		this.bool = false;
+	}
+	
+	public boolean get(){
+		return this.bool;
+	}
+}
